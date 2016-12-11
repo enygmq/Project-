e224a7bac0b78ae5c3671c98518ae5146cd5d2b0
jonathancole <jonathancole@192.168.1.5>
2016-12-11 08:19:29 -0600

    Changed main class file, added Player.java.

--- Project O/src/game/ProjectO.java
+++ Project O/src/game/ProjectO.java
@@ -1,15 +1,34 @@
 package game;
 
 import java.awt.Canvas;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
 
 public class ProjectO extends Canvas implements Runnable {
 	private static final long serialVersionUID = 1L;
+
+	public static int width = 300;
+	public static int height = width / 16 * 9;
+	public static int scale = 3;
+	
+	private static JFrame frame;
 	
 	private Thread start;
 	private boolean running = false;
+	
+	public ProjectO() {
+		frame = new JFrame("Project O – a platformer");
+	}
 
 	public void run() {
 		
+	}
+	
+	public static void main(String[] args) {
+		frame.setResizable(true);
+		frame.setVisible(true);
+		frame.setSize(new Dimension(width * scale, height * scale));
 	}
 
 }
