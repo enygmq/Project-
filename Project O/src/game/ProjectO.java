39f7d0fd2db9f1f946bdc46922a163e75577f410
jonathancole <jonathancole@192.168.1.5>
2016-12-11 08:06:40 -0600

    First change.

--- Project O/src/game/ProjectO.java
+++ Project O/src/game/ProjectO.java
@@ -1,5 +1,15 @@
 package game;
 
-public class ProjectO {
+import java.awt.Canvas;
+
+public class ProjectO extends Canvas implements Runnable {
+	private static final long serialVersionUID = 1L;
+	
+	private Thread start;
+	private boolean running = false;
+
+	public void run() {
+		
+	}
 
 }
